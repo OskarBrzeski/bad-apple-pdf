7225dfbdcac55b1b6eb4679ce1950cf9df1d95a9
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-01-28 12:39:28 +0000

    remove temp files and don't save individual images

--- src/bad_apple_pdf/main.py
+++ src/bad_apple_pdf/main.py
@@ -1,19 +1,20 @@
 from collections.abc import Iterator
+from io import BytesIO
 import os
 
 import cv2
+from cv2.typing import MatLike
 from fpdf import FPDF
-from PIL import Image
 from tqdm import tqdm
 import yt_dlp
 
 
 def download_bad_apple() -> None:
-    with yt_dlp.YoutubeDL({"outtmpl": "Bad Apple"}) as ydl:
+    with yt_dlp.YoutubeDL({"outtmpl": "Bad Apple", "paths": {"home": ".temp"}}) as ydl:
         ydl.download("https://www.youtube.com/watch?v=FtutLA63Cp8")
 
 
-def video_frames(capture: cv2.VideoCapture) -> Iterator[cv2.UMat]:
+def video_frames(capture: cv2.VideoCapture) -> Iterator[MatLike]:
     while True: 
         ret, frame = capture.read();
 
@@ -23,25 +24,28 @@ def video_frames(capture: cv2.VideoCapture) -> Iterator[cv2.UMat]:
         yield frame
 
 
-def add_image_to_pdf(pdf: FPDF, frame: cv2.UMat) -> None:
+def add_image_to_pdf(pdf: FPDF, frame: MatLike) -> None:
     pdf.add_page()
-    cv2.imwrite("currentframe.png", frame)
-    pdf.image(Image.open("currentframe.png"), x=0, y=0, w=480, h=360)
+    file = BytesIO(cv2.imencode(".png", frame)[1].tobytes())
+    pdf.image(file, x=0, y=0, w=480, h=360)
 
 
 def clean_up() -> None:
-    os.remove("Bad Apple.webm")
-    os.remove("currentframe.png")
+    os.remove(".temp/Bad Apple.webm")
+    os.rmdir(".temp")
 
 
 def main() -> None:
-    download_bad_apple()
+    if not os.path.exists(".temp"):
+        os.mkdir(".temp")
     
+    download_bad_apple()
+
     pdf = FPDF(unit="pt", format=(480, 360))
-    capture = cv2.VideoCapture("Bad Apple.webm")
+    capture = cv2.VideoCapture(".temp/Bad Apple.webm")
     frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    for frame in tqdm(video_frames(capture), desc="Bad Apple to PDF", total=frame_count):
+    for frame in tqdm(video_frames(capture), desc="Bad Apple to PDF", total=frame_count-1):
         add_image_to_pdf(pdf, frame)
 
     pdf.output("Bad Apple.pdf")
